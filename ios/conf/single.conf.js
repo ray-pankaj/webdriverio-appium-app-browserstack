444d938459d3de2bbf031113dad3cd1e6a891278
Pankaj Ahuja <ray.pankaj@gmail.com>
2018-08-31 15:51:47 +0530

    change device

--- ios/conf/single.conf.js
+++ ios/conf/single.conf.js
@@ -11,7 +11,7 @@ exports.config = {
   capabilities: [{
     name: 'single_appium_test',
     build: 'webdriver-browserstack',
-    device: 'iPhone 7',
+    device: 'iPhone 7 Plus',
     app: process.env.BROWSERSTACK_APP_ID || 'bs://<hashed app-id>',
     'browserstack.debug': true
   }],
